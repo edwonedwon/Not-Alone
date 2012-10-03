5887dbf7f44f4f0959d024111156ee16c639d287
ryan gaule <rgaule@gmail.com>
2012-10-02 17:48:10 -0700

    update with threading in the fluids...

--- 1_Unity/Assets/2_Game/Scripts/PlayerScript.cs
+++ 1_Unity/Assets/2_Game/Scripts/PlayerScript.cs
@@ -9,6 +9,11 @@ public class PlayerScript : MonoBehaviour
 	private Vector2 lastMousePos = new Vector2(0,0);
 	
 	
+	private bool mouseIsMovingWhileDown = false;
+	
+	private ArrayList currentMousePoints = new ArrayList();
+	private BlackHoleScript blackHole = null;
+	private float previousMangle = 0.0f;
 	
 	public int MouseFingerDown()
 	{
@@ -26,8 +31,70 @@ public class PlayerScript : MonoBehaviour
 	
 	void Update()
 	{
+		//DebugStreamer.message = "mouseIsMovingWhileDown: " + mouseIsMovingWhileDown.ToString();
 		
 		
+		if(blackHole == null)
+		{
+			GameObject go = GameObject.FindGameObjectWithTag("blackhole");
+			if(go != null)
+				blackHole = go.GetComponent<BlackHoleScript>();
+			
+		}
+		
+		
+		if(mouseIsMovingWhileDown && blackHole != null)
+		{
+			Camera camcam = Camera.main;
+			
+			
+			Vector3 blackHoleCenter = camcam.WorldToScreenPoint(blackHole.transform.position);
+			
+			//DebugStreamer.message = "currentMousePoints: " + currentMousePoints.Count.ToString();
+			
+			float compundedMangle = 0.0f;
+            float totalmangle = 0.0f;
+
+            float direction = 1;
+
+            for (int i = 1; i < currentMousePoints.Count; ++i)
+            {
+                Vector2 old = (Vector2)currentMousePoints[i - 1];
+                Vector2 cur = (Vector2)currentMousePoints[i];
+
+                float yDistCur = cur.y - blackHoleCenter.y;
+                float xDistCur = cur.x - blackHoleCenter.x;
+
+                float yDistOld = old.y - blackHoleCenter.y;
+                float xDistOld = old.x - blackHoleCenter.x;
+
+                float maxRadius = 100;
+                float minRadius = 20;
+
+                float curLen = (float)System.Math.Sqrt((xDistCur * xDistCur) + (yDistCur * yDistCur));
+                float oldLen = (float)System.Math.Sqrt((xDistOld * xDistOld) + (yDistOld * yDistOld));
+
+                if (curLen > minRadius && curLen < maxRadius)
+                {
+                    float degsCur = ToDegrees((float)System.Math.Atan2(yDistCur, xDistCur));
+                    float degresOld = ToDegrees((float)System.Math.Atan2(yDistOld, xDistOld));
+
+                    if (System.Math.Sign(degresOld) == System.Math.Sign(degsCur))
+                    {
+                        compundedMangle += System.Math.Abs(System.Math.Abs(degsCur) - System.Math.Abs(degresOld));
+                        totalmangle += degsCur - degresOld;
+                    }
+                }
+            }
+
+            if(totalmangle < 0)
+                direction = -1;
+			
+			
+			blackHole.RotationSpeed += (totalmangle-previousMangle) * 0.1f;
+			previousMangle = totalmangle;
+			//DebugStreamer.message = "totalmangle: " + totalmangle.ToString();
+		}		
 	}
 	
 	#region Events
@@ -35,6 +102,12 @@ public class PlayerScript : MonoBehaviour
 	void OnEnable()
 	{
 		networkView.observed = this;
+	}
+	
+	
+	public float ToDegrees(float radians)
+	{
+		return (float)(radians * (180.0 / 3.14159265359f));
 	}
 	
 	void OnDisable()
@@ -68,25 +141,34 @@ public class PlayerScript : MonoBehaviour
 	
 	#region Finger Gestures
 	
-	
-	
 	public void OnPlayerFingerDown (int finger, Vector2 pos)
 	{
-		mouseFingerDown = finger;	//either 0, or 1 i believe..
+		
+		currentMousePoints.Add(pos);
 		lastMousePos = pos;
 		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(lastMousePos.x, lastMousePos.y, zOffset));
+		mouseFingerDown = finger;	//either 0, or 1 i believe..
+		mouseIsMovingWhileDown = true;
 	}
 	
 	public void OnPlayerFingerMove (int finger, Vector2 pos)
 	{
-		mouseFingerDown = finger;	//either 0, or 1 i believe..
+		//DebugStreamer.message = "mouse move pos: " + pos.ToString();
+		
+		currentMousePoints.Add(pos);
 		lastMousePos = pos;
 		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(lastMousePos.x, lastMousePos.y, zOffset));
+		mouseFingerDown = finger;	//either 0, or 1 i believe..
 	}
 	
 	public void OnPlayerFingerUp (int finger, Vector2 pos, float timeHeldDown)
 	{
-		mouseFingerDown = -1;	//up!		
+		mouseIsMovingWhileDown = false;
+		mouseFingerDown = -1;	//up!
+		
+		
+		previousMangle = 0;
+		currentMousePoints.Clear();
 	}
 	
 	
