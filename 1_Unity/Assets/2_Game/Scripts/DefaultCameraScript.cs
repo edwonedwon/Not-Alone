dfaca78243991430ea35d50c80a51ceb8c1975e5
rgaule <rgaule@gmail.com>
2012-10-10 16:23:26 -0700

    default camera script

---
+++ 1_Unity/Assets/2_Game/Scripts/DefaultCameraScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class DefaultCameraScript : MonoBehaviour
+{
+	public FluidFieldGenerator field = null;
+	
+	
+	
+	void Start ()
+	{
+	
+	}
+	
+
+	void Update ()
+	{
+	
+	}
+	
+	void OnPostRender()
+	{
+		
+		field.PostRenderParticles();		
+	}
+}
